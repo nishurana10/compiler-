b6ad454a2170c6aa4bd2e26bd71943197d745210
nishurana10 <91240343+nishurana10@users.noreply.github.com>
2021-11-19 19:07:04 +0530

    Create Write a program to check the string is ‘a’,’aa’,…..,’aaaaa..’@nishu.c

---
+++ Write a program to check the string is ‘a’,’aa’,…..,’aaaaa..’@nishu.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main(){
+char s[100]; int i,n, flag=0;
+clrscr();
+printf("Nishu Rana\n2001330109016\n\n\nEnter the string:\n");
+scanf("%s", &s);
+n=strlen(s);
+for(i=0;i<n;i++){
+if(s[i]!='a')
+flag=1;
+}
+if(flag==0)
+printf("Yes string has all a");
+else
+printf("no string doesnt have all a");
+getch();
+}
